5d793c1c93e393b6cbd7ac06add08aee206bf42a
Richie Barajas <rb1.mx5@gmail.com>
2022-11-10 23:03:26 -0700

    Modified computers choice function and created a function to run single game of RPS and print results

--- RPS_JS.js
+++ RPS_JS.js
@@ -1,7 +1,30 @@
-function getComputerChoice(choices) 
+
+
+
+function getComputerChoice() 
 {
+    const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-var choices = ["Rock", "Paper", "Scissors"];
-console.log(getComputerChoice(choices));
+
+const playerSelection = prompt("Pleaes choose rock, paper, or scissors: ".toLowerCase());
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection)
+{
+    if (playerSelection === computerSelection) {
+        return ("Its a tie! You both picked " + playerSelection);
+    } else  if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper";
+    } else {
+        return ("You lose! " + playerSelection + " loses to " + computerSelection);
+    }
+}
+
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
