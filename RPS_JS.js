876b052ddfb4dcee9fa4293009107f8261ae0ed9
Richie Barajas <rb1.mx5@gmail.com>
2022-11-15 19:09:58 -0700

    Reworked game function so it would loop, Need to fix score tracker and make sure ties are not counted as wins

--- RPS_JS.js
+++ RPS_JS.js
@@ -1,22 +1,20 @@
 
 
-function getComputerChoice() 
-{
+function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random()*choices.length)];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 
-const playerSelection = prompt("Please choose rock, paper, or scissors: ");
-const newPlayerSelection = playerSelection.toLowerCase();
+let playerSelection = prompt("Please choose rock, paper, or scissors: ");
+let newPlayerSelection = playerSelection.toLowerCase();
 
-function playRound(newPlayerSelection, computerSelection)
-{
-    const playerSelection = newPlayerSelection();
-    const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
+
+function playRound(newPlayerSelection, computerSelection) {
     if (newPlayerSelection === computerSelection) {
         return ("Its a tie! You both picked " + newPlayerSelection);
-    } else  if (newPlayerSelection === "rock" && computerSelection === "scissors") {
+    } else if (newPlayerSelection === "rock" && computerSelection === "scissors") {
         return "You win! rock beats scissors";
     } else if (newPlayerSelection === "paper" && computerSelection === "rock") {
         return "You win! paper beats rock";
@@ -27,13 +25,42 @@ function playRound(newPlayerSelection, computerSelection)
     }
 }
 
-console.log(computerSelection);
-console.log(playRound(newPlayerSelection, computerSelection));
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+let roundResult = playRound();
 
-function game(){
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
+function game() {
+    for (let i = 0; i < 4; i++) { //what is going to be loop
+        playerSelection = prompt("lets play rock papper scissors");
+
+        console.log("you choose " + playerSelection.toLowerCase());
+
+        console.log("The computer choose " + computerSelection);
+
+        console.log(playRound(playerSelection, computerSelection));
+
+        playRound();
+
+        if (roundResult === 'You win! rock beats scissors' || 'You win! paper beats rock' || 'You win! scissors beats paper') {
+            ++playerScore;
+        } else if (roundResult === 'You lose! rock loses to paper' || 'You lose! paper loses to scissors' || 'You lose! scissors loses to rock') {
+            ++computerScore;
+        } else {
+            ++tieScore;
+        }
+    }
+    if (playerScore > computerScore) {
+        return "You Win this game.\n -------";
+    }
+    if (playerScore == computerScore) {
+        return "This game is a tie.\n -------";
+    }
+    else {
+        return "You Lose this game.\n -------";
     }
 }
 
-game();
+console.log(game());
+
+console.log("The final score is " + playerScore + " to " + computerScore + " and this many ties " + tieScore);
