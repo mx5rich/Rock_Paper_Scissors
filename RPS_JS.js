09844eb0cb9d53263f6754fcc53c946d056e92f1
Richie Barajas <rb1.mx5@gmail.com>
2022-11-20 14:48:07 -0700

    Finished RPS_JS.js file and completed the task of running a five round game and printing the results

--- RPS_JS.js
+++ RPS_JS.js
@@ -5,64 +5,62 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-
-// let playerSelection = prompt("Please choose rock, paper, or scissors: ");  let newPlayerSelection = playerSelection.toLowerCase(); //
-
-let computerSelection = getComputerChoice();
-
-function playRound(newPlayerSelection, computerSelection) {
-    if (newPlayerSelection === computerSelection) {
-        return ("Its a tie! You both picked " + newPlayerSelection);
-    } else if (newPlayerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! rock beats scissors";
-    } else if (newPlayerSelection === "paper" && computerSelection === "rock") {
-        return "You win! paper beats rock";
-    } else if (newPlayerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! scissors beats paper";
-    } else {
-        return ("You lose! " + newPlayerSelection + " loses to " + computerSelection);
-    }
-}
-
 let playerScore = 0;
 let computerScore = 0;
 let tieScore = 0;
 
+let playerSelection = prompt("Please choose rock, paper, or scissors: ");  
+let newPlayerSelection = playerSelection.toLowerCase(); 
+
+let computerSelection = getComputerChoice();
+
+function playRound(newPlayerSelection, computerSelection) {
+
+    if (newPlayerSelection === computerSelection) {
+        ++tieScore;
+        return ("Its a tie! You both picked " + newPlayerSelection);
+    } else if (newPlayerSelection === "rock" && computerSelection === "scissors") {
+        ++playerScore;
+        return "You win! rock beats scissors";
+    } else if (newPlayerSelection === "paper" && computerSelection === "rock") {
+        ++playerScore;
+        return "You win! paper beats rock";
+    } else if (newPlayerSelection === "scissors" && computerSelection === "paper") {
+        ++playerScore;
+        return "You win! scissors beats paper";
+    } else {
+        ++computerScore;
+        return ("You lose! " + newPlayerSelection + " loses to " + computerSelection);
+    }
+}
+
 function game() {
-    let roundResult = playRound();
-    for (let i = 0; i < 4; i++) { 
-        playerSelection = prompt("lets play rock papper scissors");
+    for (let i = 0; i < 5; i++) { 
+        let playerSelection = prompt("Please choose rock, paper, or scissors: ");  
+        let newPlayerSelection = playerSelection.toLowerCase(); 
+
+        let computerSelection = getComputerChoice();
 
         console.log("you choose " + playerSelection.toLowerCase());
 
         console.log("The computer choose " + computerSelection);
 
-       console.log(playRound(playerSelection, computerSelection)); 
-
-        playRound();
-
-        if (roundResult === 'You win! rock beats scissors' || 'You win! paper beats rock' || 'You win! scissors beats paper') {
-            ++playerScore;
-        } else if (roundResult === 'You lose! rock loses to paper' || 'You lose! paper loses to scissors ' || 'You lose! scissors loses to rock') {
-            ++computerScore;
-        } else {
-            ++tieScore;
-        }
+        console.log(playRound(playerSelection, computerSelection)); 
     }
 
-        console.log("Player score: " + playerScore + " ComputerScore: " + computerScore);
+        console.log("Player score: " + playerScore + " ComputerScore: " + computerScore + " TieScore: " + tieScore);
         
     if (playerScore > computerScore) {
-        return "You Win this game.\n -------";
+        return "You Win this game.";
     }
     if (playerScore == computerScore) {
-        return "This game is a tie.\n -------";
+        return "This game is a Tie.";
     }
     else {
-        return "You Lose this game.\n -------";
+        return "You Lose this game.";
     }
 }
 
-console.log(game(playRound()));
+console.log(game());
 
 console.log("The final score is " + playerScore + " to " + computerScore + " and this many ties " + tieScore);
